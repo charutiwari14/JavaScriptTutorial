5413201e8e4ced9cfb3799f7518f2d44d95a92f7
charutiwari14 <73792150+charutiwari14@users.noreply.github.com>
2025-04-17 11:17:00 +0000

    Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,38 @@
+"use strict"; //Treat all JS code as newer version
+
+// alert(4+6) We are using node js not browser
+
+// console.log(4+7); console.log("Banana"); //Ewww gndi readability
+
+// console.log(4+7); 
+// console.log("Banana");
+// //Yea now thats cute
+
+
+let name = "Charu"
+let age=23
+let idLoggedIn=true
+let state;//undefined
+let nulldt=null
+
+//number
+//bigint
+//string=>""
+//boolean=> true/false
+//null=>Standalone value lets say
+//undefined
+//symbol=> //unique h // react n components mai useful hota hai
+
+
+//object
+
+// console.log(typeof 6);
+// console.log(typeof age);
+// console.log(typeof state);
+// console.log(typeof name); 
+
+console.log(typeof undefined); //=>undefined
+console.log(typeof null); //=>Object
+
+
+
