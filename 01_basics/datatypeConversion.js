ee6e727497b97268dfb109c29a314fd1683ff3cb
charutiwari14 <73792150+charutiwari14@users.noreply.github.com>
2025-04-18 06:14:06 +0000

    Conversions

---
+++ 01_basics/datatypeConversion.js
@@ -0,0 +1,46 @@
+//(1)// let score="50"
+
+// console.log(typeof (score));//=>String
+
+// let valueInNumber=Number(score)//=>Converted to number and stored in new variable
+// console.log(typeof (valueInNumber));//=>Only type is changed,Value will be unknown
+
+
+// //(2)let score="50mew" 
+// let score=false
+// console.log(typeof (score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof (valueInNumber)); 
+// console.log(valueInNumber);
+
+/*
+NOTES now
+someType=>On converting to Number
+33=>33
+"63"=>63
+"anything"=>NaN
+true=>1;false=>0;
+null=>0
+undefined=>NaN
+*/
+
+// let isLoggedIn="charu"
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+// console.log(typeof isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//1=>true 0=>false
+//""=>false
+//"anything"=>true
+
+let koisaNumber=14
+
+let stringKoisaNumber=String(koisaNumber)
+console.log(stringKoisaNumber);
+console.log(typeof stringKoisaNumber);
+
+
+
+
