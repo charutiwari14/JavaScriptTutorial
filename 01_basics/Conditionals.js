7322aaf9eb7b5eb186d55323336978c7632686d4
charutiwari14 <73792150+charutiwari14@users.noreply.github.com>
2025-04-18 10:45:38 +0000

    Comparisons and Equality

---
+++ 01_basics/Conditionals.js
@@ -0,0 +1,28 @@
+// console.log(2>1);//Same data type comparison
+
+
+//Comparing different datatypes
+
+// console.log("2">1);
+// console.log("02">1);
+
+
+// Line 11 to 17 are not recommended cause null or undefined would either get converted to 0 or NaN we never know
+// console.log(null>0);
+// console.log(null==0);//Equality operator does no convert null to 0 only comparison operators do that
+// console.log(null<=0);
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined<=0);
+
+
+console.log("2"==2);
+console.log(true==1);
+
+
+//Strict check === It checks data type as well as value
+console.log("2"===2);
+
+
+
